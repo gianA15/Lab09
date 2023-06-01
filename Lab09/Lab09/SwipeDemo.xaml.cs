41258c0aaacc21eff21bb850c63b3101a42754e4
gianA15 <gian.arce@tecsup.edu.pe>
2023-05-31 19:55:17 -0500

    Agregar archivos de proyecto.

---
+++ Lab09/Lab09/SwipeDemo.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab09
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SwipeDemo : ContentPage
+    {
+        public SwipeDemo()
+        {
+            InitializeComponent();
+        }
+
+        void OnSwiped(object sender, SwipedEventArgs e)
+        {
+            string direction = e.Direction.ToString();
+
+
+            if (direction == "Right")
+                ((BoxView)sender).BackgroundColor = Color.Red;
+            if (direction == "Left")
+                ((BoxView)sender).BackgroundColor = Color.Yellow;
+            if (direction == "Up")
+                ((BoxView)sender).BackgroundColor = Color.Green;
+            if (direction == "Down")
+                ((BoxView)sender).BackgroundColor = Color.Blue;
+
+            _label.Text = $"You swiped: {e.Direction.ToString()}";
+        }
+    }
+}
